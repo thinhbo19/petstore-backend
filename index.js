4c5941fdf6a7fa4533efb5d498a8f795a7286d2c
thinhbo19 <111267094+thinhbo19@users.noreply.github.com>
2024-09-09 00:38:36 +0700

    error

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const app = express();
+
 app.use(
   cors({
     origin: process.env.URL_CLIENT,
     methods: ["POST", "PUT", "GET", "DELETE", "PATCH"],
   })
 );
+
 app.use(cookieParser());
 const port = process.env.PORT || 8888;
 app.use(express.json());
